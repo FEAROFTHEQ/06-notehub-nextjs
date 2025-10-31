b7117373a6479d5a3e8cb54152d89d3389eb7f2c
Alina Y. <devadevam.tod@gmail.com>
2025-10-31 18:05:56 +0200

    done NoteClient

--- app/notes/App.tsx
+++ app/notes/App.tsx
@@ -2,30 +2,27 @@
 
 import { useState } from "react";
 import css from "./App.module.css";
-import { keepPreviousData, useQuery } from "@tanstack/react-query";
 // import toast, { Toaster } from "react-hot-toast";
-import { fetchNotes } from "../../lib/api";
 import NoteList from "../../components/NoteList/NoteList";
 import Pagination from "../../components/Pagination/Pagination";
 import Modal from "../../components/Modal/Modal";
 import NoteForm from "../../components/NoteForm/NoteForm";
 import SearchBox from "../../components/SearchBox/SearchBox";
 import { useDebouncedCallback } from "use-debounce";
+import { Note } from "@/types/note";
 
-function App() {
-  const [page, setPage] = useState(1);
-  const [query, setQuery] = useState("");
+interface AppProps {
+  setPage: (page: number) => void;
+  setQuery: (query: string) => void;
+  notesData: { notes: Note[]; totalPages: number };
+  page: number;
+}
+
+function App({ setPage, setQuery, notesData, page }: AppProps) {
   const [isModal, setIsModal] = useState(false);
 
   const openModal = () => setIsModal(true);
   const closeModal = () => setIsModal(false);
-
-  const { data: notesData } = useQuery({
-    queryKey: ["notes", query, page],
-    queryFn: () => fetchNotes(query, page),
-    placeholderData: keepPreviousData,
-    refetchOnMount: false,
-  });
 
   const handleSearch = useDebouncedCallback((query: string) => {
     setQuery(query);
--- app/notes/Notes.client.tsx
+++ app/notes/Notes.client.tsx
@@ -1,7 +1,30 @@
 "use client";
 
+import { fetchNotes } from "@/lib/api";
 import App from "./App";
+import { keepPreviousData, useQuery } from "@tanstack/react-query";
+import { useState } from "react";
 
 export default function NoteClient() {
-  return <App />;
+  const [page, setPage] = useState(1);
+  const [query, setQuery] = useState("");
+  const { data: notesData, isLoading } = useQuery({
+    queryKey: ["notes", query, page],
+    queryFn: () => fetchNotes(query, page),
+    placeholderData: keepPreviousData,
+    refetchOnMount: false,
+  });
+  return (
+    <>
+      {isLoading && <p>Loading your notes...</p>}
+      {!isLoading && notesData && (
+        <App
+          setPage={setPage}
+          setQuery={setQuery}
+          notesData={notesData}
+          page={page}
+        />
+      )}
+    </>
+  );
 }
