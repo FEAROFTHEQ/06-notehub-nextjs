1d57fbee0682aeae86de3a2c0f8862cd969ace9c
Alina Y. <devadevam.tod@gmail.com>
2025-11-02 00:09:44 +0200

    fix types, notes.client

--- app/notes/Notes.client.tsx
+++ app/notes/Notes.client.tsx
@@ -1,9 +1,15 @@
 "use client";
 
 import { fetchNotes } from "@/lib/api";
-import App from "./App";
 import { keepPreviousData, useQuery } from "@tanstack/react-query";
 import { useState } from "react";
+import { useDebouncedCallback } from "use-debounce";
+import css from "./App.module.css";
+import NoteList from "../../components/NoteList/NoteList";
+import Pagination from "../../components/Pagination/Pagination";
+import Modal from "../../components/Modal/Modal";
+import NoteForm from "../../components/NoteForm/NoteForm";
+import SearchBox from "../../components/SearchBox/SearchBox";
 
 export default function NoteClient() {
   const [page, setPage] = useState(1);
@@ -18,17 +24,45 @@ export default function NoteClient() {
     placeholderData: keepPreviousData,
     refetchOnMount: false,
   });
+
+  const [isModal, setIsModal] = useState(false);
+
+  const openModal = () => setIsModal(true);
+  const closeModal = () => setIsModal(false);
+
+  const handleSearch = useDebouncedCallback((query: string) => {
+    setQuery(query);
+    setPage(1);
+  }, 300);
+
   return (
     <>
       {isLoading && <p>Loading your notes...</p>}
       {isError && <p>Sorry, error happened...</p>}
       {!isLoading && !isError && notesData && (
-        <App
-          setPage={setPage}
-          setQuery={setQuery}
-          notesData={notesData}
-          page={page}
-        />
+        <div className={css.app}>
+          <header className={css.toolbar}>
+            <SearchBox onSearch={handleSearch} />
+            {notesData.totalPages > 1 && (
+              <Pagination
+                totalPages={notesData.totalPages}
+                page={page}
+                setPage={setPage}
+              />
+            )}
+            <button className={css.button} onClick={openModal}>
+              Create note +
+            </button>
+            {isModal && (
+              <Modal onClose={closeModal}>
+                <NoteForm setPage={setPage} closeModal={closeModal} />
+              </Modal>
+            )}
+          </header>
+          {notesData && notesData.notes.length > 0 && (
+            <NoteList notes={notesData.notes} />
+          )}
+        </div>
       )}
     </>
   );
